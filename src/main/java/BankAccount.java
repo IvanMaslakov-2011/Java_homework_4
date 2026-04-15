2792c7dabb37716b543b830e3fdce02d72610542
Ivan Maslakov <0500456367v@gmail.com>
2026-04-15 22:45:43 +0300

    BankAccount

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,5 +1,14 @@
 public class BankAccount {
 
+    public static void main(){
+
+        BankAccount bankAccount1 = new BankAccount("1", "Vanya");
+        bankAccount1.deposit(1000);
+        bankAccount1.withdraw(100);
+        bankAccount1.displayInfo();
+
+    }
+
     String accountNumber;
     String ownerName;
     private double balance;
@@ -22,6 +31,14 @@ public class BankAccount {
             return true;
         }
         return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void displayInfo(){
+        System.out.println("Номер аккаунту: " + accountNumber + "\n Ім'я власника: " + ownerName + "\nБаланс: " + balance);
     }
 
 
