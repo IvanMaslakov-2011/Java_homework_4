0acec4a6d2db02425b46b45d42033949a9ccd751
Ivan Maslakov <0500456367v@gmail.com>
2026-04-16 22:01:38 +0300

    Fraction

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -3,7 +3,22 @@ public class Fraction {
     private int numerator;
     private int denominator;
 
-    public static void main(String[] args){
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        setDenominator(denominator);
+
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        int gcd = gcd(Math.abs(numerator), denominator);
+
+        this.numerator = (numerator / gcd);
+        this.denominator = (denominator / gcd);
+    }
+
+    public static void main(String[] args) {
         Fraction fraction1 = new Fraction(1, 2);
         Fraction fraction2 = new Fraction(30, 6);
 
@@ -16,21 +31,6 @@ public class Fraction {
 
     }
 
-    public Fraction(int numerator, int denominator){
-        this.numerator = numerator;
-        setDenominator(denominator);
-
-        if (denominator < 0){
-            numerator = -numerator;
-            denominator = -denominator;
-        }
-
-        int gcd = gcd(Math.abs(numerator), denominator);
-
-        this.numerator = (numerator / gcd);
-        this.denominator = (denominator / gcd);
-    }
-
     public void setDenominator(int denominator) {
         if (denominator != 0) {
             this.denominator = denominator;
@@ -39,15 +39,15 @@ public class Fraction {
         }
     }
 
-    private int gcd(int a, int b){
-        if (b == 0){
+    private int gcd(int a, int b) {
+        if (b == 0) {
             return a;
         }
 
         return gcd(b, a % b);
     }
 
-    public Fraction addFraction(Fraction other){
+    public Fraction addFraction(Fraction other) {
 
         int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
         int newDenominator = this.denominator * other.denominator;
@@ -55,23 +55,23 @@ public class Fraction {
         return new Fraction(newNumerator, newDenominator);
     }
 
-    public Fraction multiply(Fraction other){
-        int newNumerator =  this.numerator * other.numerator;
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
         int newDenominator = this.denominator * other.denominator;
 
         return new Fraction(newNumerator, newDenominator);
     }
 
-    public double toDecimal(){
+    public double toDecimal() {
         return (double) numerator / denominator;
     }
 
-    public void displayInfo(){
+    public void displayInfo() {
         System.out.println(numerator + "/" + denominator);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return numerator + "/" + denominator;
     }
 }
