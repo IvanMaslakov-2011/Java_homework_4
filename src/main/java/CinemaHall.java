adeee4bdaa21d6820d7ebca01b809da30f429827
Ivan Maslakov <0500456367v@gmail.com>
2026-04-16 21:49:18 +0300

    CinemaHall

--- src/main/java/CinemaHall.java
+++ src/main/java/CinemaHall.java
@@ -1,6 +1,4 @@
 class Seat {
-
-
     private int row;
     private int number;
     private boolean isBooked;
@@ -39,29 +37,17 @@ class Seat {
 
 public class CinemaHall {
 
-    public static void main(String[] args){
-
-        CinemaHall cinemaHall1 = new CinemaHall(5, 5);
-
-        cinemaHall1.bookSeat(1, 4);
-        cinemaHall1.bookSeat(3,1);
-        cinemaHall1.bookSeat(5,5);
-        cinemaHall1.printSeatingPlan();
-        cinemaHall1.countAvailableSeats();
-    }
-
     private int rows;
     private int seatsPerRow;
     private Seat[][] seats;
-
     public CinemaHall(int rows, int seatsPerRow) {
         setRows(rows);
         setSeatsPerRow(seatsPerRow);
 
         this.seats = new Seat[rows][seatsPerRow];
 
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < seatsPerRow; j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seatsPerRow; j++) {
                 this.seats[i][j] = new Seat(i + 1, j + 1, false);
             }
         }
@@ -69,31 +55,42 @@ public class CinemaHall {
 
     }
 
+    public static void main(String[] args) {
+
+        CinemaHall cinemaHall1 = new CinemaHall(5, 5);
+
+        cinemaHall1.bookSeat(1, 4);
+        cinemaHall1.bookSeat(3, 1);
+        cinemaHall1.bookSeat(5, 5);
+        cinemaHall1.printSeatingPlan();
+        cinemaHall1.countAvailableSeats();
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
     public void setRows(int rows) {
         this.rows = rows;
+    }
+
+    public int getSeatsPerRow() {
+        return seatsPerRow;
     }
 
     public void setSeatsPerRow(int seatsPerRow) {
         this.seatsPerRow = seatsPerRow;
     }
 
-    public int getRows(){
-        return rows;
-    }
-
-    public int getSeatsPerRow(){
-        return seatsPerRow;
-    }
-
     public void bookSeat(int row, int seat) {
         if (row >= 1 && row <= rows && seat >= 1 && seat <= seatsPerRow) {
 
-            Seat targerSeat = seats[row - 1] [seat - 1];
+            Seat targerSeat = seats[row - 1][seat - 1];
 
-            if (!targerSeat.isBooked()){
+            if (!targerSeat.isBooked()) {
                 targerSeat.setIsBooked(true);
                 System.out.println("Місце: " + seat + " у ряду: " + row + " успішно заброньованно!");
-            } else{
+            } else {
                 System.out.println("Вибачте, але це місце вже зайняте!");
             }
         } else {
@@ -104,12 +101,12 @@ public class CinemaHall {
     public void cancelBooking(int row, int seat) {
         if (row >= 1 && row <= rows && seat >= 1 && seat <= seatsPerRow) {
 
-            Seat targerSeat = seats[row - 1] [seat - 1];
+            Seat targerSeat = seats[row - 1][seat - 1];
 
-            if (targerSeat.isBooked()){
+            if (targerSeat.isBooked()) {
                 targerSeat.setIsBooked(false);
                 System.out.println("Місце: " + seat + " у ряду: " + row + " бронювання успішно відміненно!");
-            } else{
+            } else {
                 System.out.println("Вибачте, але це місце ще не заброньованне!");
             }
         } else {
@@ -117,14 +114,14 @@ public class CinemaHall {
         }
     }
 
-    public void printSeatingPlan(){
+    public void printSeatingPlan() {
         System.out.println("План залу");
 
-        for (int i = 0; i < rows; i++){
+        for (int i = 0; i < rows; i++) {
             System.out.println("Ряд: " + (i + 1) + ": ");
 
-            for (int j = 0; j < seatsPerRow; j++){
-                if (seats[i][j].isBooked()){
+            for (int j = 0; j < seatsPerRow; j++) {
+                if (seats[i][j].isBooked()) {
                     System.out.print("[X]");
                 } else {
                     System.out.print("[]");
@@ -134,13 +131,13 @@ public class CinemaHall {
         }
     }
 
-    public int countAvailableSeats(){
+    public int countAvailableSeats() {
 
         int availableCount = 0;
 
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < seatsPerRow; j++){
-                if (!seats[i][j].isBooked()){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seatsPerRow; j++) {
+                if (!seats[i][j].isBooked()) {
                     availableCount++;
                 }
             }
