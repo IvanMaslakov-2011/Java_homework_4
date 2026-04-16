dfe56458a8c87479da45224721f256ba96d7110d
Ivan Maslakov <0500456367v@gmail.com>
2026-04-16 21:55:45 +0300

    BankAccount

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,23 +1,22 @@
 public class BankAccount {
 
-    public static void main(String[] args){
+    String accountNumber;
+    String ownerName;
+    private double balance;
+    public BankAccount(String accountNumber, String ownerName) {
+
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = 0;
+    }
+
+    public static void main(String[] args) {
 
         BankAccount bankAccount1 = new BankAccount("1", "Vanya");
         bankAccount1.deposit(1000);
         bankAccount1.withdraw(100);
         bankAccount1.displayInfo();
 
-    }
-
-    String accountNumber;
-    String ownerName;
-    private double balance;
-
-    public BankAccount(String accountNumber, String ownerName) {
-
-        this.accountNumber = accountNumber;
-        this.ownerName = ownerName;
-        this.balance = 0;
     }
 
     public void deposit(double amount) {
@@ -37,7 +36,7 @@ public class BankAccount {
         return balance;
     }
 
-    public void displayInfo(){
+    public void displayInfo() {
         System.out.println("Номер аккаунту: " + accountNumber + "\n Ім'я власника: " + ownerName + "\nБаланс: " + balance);
     }
 
