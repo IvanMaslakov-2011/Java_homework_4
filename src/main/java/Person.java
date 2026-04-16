7d1a9d4f48f9745097b6ac19da843b7c01ce0c79
Ivan Maslakov <0500456367v@gmail.com>
2026-04-16 21:58:56 +0300

    Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -4,15 +4,15 @@ public class Person {
     private int age;
     private double moneyUAH;
 
-    public static void main(String[] args){
-        Person person1 = new Person("Joe", 15, 10000);
-        person1.converToUSD(42);
-    }
-
-    public Person(String name, int age, double moneyUAH){
+    public Person(String name, int age, double moneyUAH) {
         this.name = name;
         setAge(age);
         setMoneyUAH(moneyUAH);
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Joe", 15, 10000);
+        person1.converToUSD(42);
     }
 
     public String getName() {
@@ -23,28 +23,28 @@ public class Person {
         return age;
     }
 
-    public double getMoneyUAH() {
-        return moneyUAH;
-    }
-
-    public void setAge(int age){
-        if (age >= 0){
+    public void setAge(int age) {
+        if (age >= 0) {
             this.age = age;
         } else {
             throw new IllegalArgumentException("Вік не може бути від'ємним!");
         }
     }
 
-    public void setMoneyUAH(double moneyUAH){
-        if (moneyUAH >= 0){
+    public double getMoneyUAH() {
+        return moneyUAH;
+    }
+
+    public void setMoneyUAH(double moneyUAH) {
+        if (moneyUAH >= 0) {
             this.moneyUAH = moneyUAH;
         } else {
             throw new IllegalArgumentException("Кількість грошей не може бути менше нуля!");
         }
     }
 
-    public void converToUSD(double rate){
-        if (rate > 0){
+    public void converToUSD(double rate) {
+        if (rate > 0) {
             System.out.println("Ваші гроші у USD: " + (moneyUAH / rate));
         } else {
             throw new IllegalArgumentException("Курс має бути більше нуля!");
